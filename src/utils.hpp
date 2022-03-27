533bd72dde4ba063e957a399a2e387eee6738f0a
klanc <clank201@gmail.com>
2022-03-27 16:41:40 +0200

    Fuck it, remove new/delete overrides

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,23 +1,6 @@
 #pragma once
 #include "FreeRTOS.h"
 #include <stdint.h>
-#include <cstddef>
-
-void* operator new(size_t size){
-    return pvPortMalloc(size);
-}
-
-void* operator new[](size_t size){
-    return pvPortMalloc(size);
-}
-
-void operator delete(void* ptr){
-    vPortFree(ptr);
-}
-
-void operator delete[](void* ptr){
-    vPortFree(ptr);
-}
 
 const uint32_t TEMP_PROBE_PIN = 26;
 const uint32_t HEATER_PWM_PIN = 6;
